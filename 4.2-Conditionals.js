1c8a3edfba2a84af5d82fdcddd3d2993d0d5b0d6
marynyambura <nyamburamary650@gmail.com>
2025-12-15 11:27:54 +0300

    added changes in conditionals

--- 4.2-Conditionals.js
+++ 4.2-Conditionals.js
@@ -4,7 +4,7 @@ if ( age >=18){
     console.log("you are an aldult")
 }
 
-//if and else statement
+//if and else statement(BASIC CONDITIONALS)
 let marks = 40;
 
 if (marks >= 50) {
@@ -12,5 +12,27 @@ if (marks >= 50) {
 } else {
   console.log("Fail");
 }
+//the ouput will be  "fail"
 
+// if statement 
+let isHungry = true;
 
+if (isHungry) {
+  console.log("Eat food");
+}
+
+console.log("Continue with day");
+// the if statement is true so it will print 'Eat food' and Continue with your day
+
+// else if (multiple choices)
+let score = 70
+if (  score >= 75) {
+  console.log( "Grade A")
+}
+else if (score>=60){
+  console.log("Grade B")
+}
+else{
+  console.log("Fail")
+}
+//this will print Grade B since the score fall it falls in that category
