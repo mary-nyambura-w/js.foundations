07834fd03e007208d390a057951d244e40a5975e
marynyambura <nyamburamary650@gmail.com>
2025-12-15 20:21:57 +0300

    added changes in loops

---
+++ 4.3-Loops.js
@@ -0,0 +1,35 @@
+//for loop(common )
+// for (first, condition,change ){code to repeat}
+for (let i = 0; i < 3; i++) {
+  // Code to be executed 3 times (i = 0, 1, 2)
+}
+
+for (let x = 1; x <= 3; x++) {
+  console.log("hello");
+}
+
+
+//while
+ let  i = 1;
+
+while (i <= 3) {
+  console.log("Hi");
+  i++;
+}
+
+
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    break;
+  }
+  console.log(i);
+}
+
+
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    continue;
+  }
+  console.log(i);
+}
+
