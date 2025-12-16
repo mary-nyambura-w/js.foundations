d75ca6bac42c135d8015ce3355410078396345c6
marynyambura <nyamburamary650@gmail.com>
2025-12-16 11:31:48 +0300

    number  3

--- exercises.js
+++ exercises.js
@@ -1,13 +1,24 @@
 //create a function that takes a name as the parameter and returns a greeting message
- function greet(name) {
+function greet(name) {
   console.log("Hello " + name);
 }
 
 greet("Mary");
- 
+
 function add(a, b) {
   return a + b;
 }
 let result = add(2, 3);
 console.log(result);
 
+ // Function to check even or odd
+function checkEvenOdd(number) {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+console.log(checkEvenOdd(4)); // Even
+console.log(checkEvenOdd(7)); // Odd
+ 
